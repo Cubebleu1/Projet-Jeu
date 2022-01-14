35314e9eaa0d2405ff914b1bb7db2add6393029b
Raphael-Caudron <91120730+Raphael-Caudron@users.noreply.github.com>
2022-01-14 15:46:04 +0100

    Create mechant.py

---
+++ Jeu/mechant.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 14 15:08:30 2022
+
+@author: rapha
+"""
+from vaisseau import vaisseau
+#Mechant de base, il ne tire pas.
+class mechant(vaisseau):
+    def __init__(self, gui,couleur, POSX, POSY, tag):
+        super().__init__(gui, couleur, POSX, POSY, tag)
+        self.__img="alienNoir.jpg"
+        self.stats = (1, 1, 10)
+        
+    def behavior(self):
+        pass
